c559e7333fc902ca236a169dbb63be71422f69be
Tomas Longo <longo.tomas@googlemail.com>
2014-09-26 08:42:37 +0200

    LogService is now managed by spring

--- src/main/java/de/tlongo/serveranalytics/services/logfileservice/LogService.java
+++ src/main/java/de/tlongo/serveranalytics/services/logfileservice/LogService.java
@@ -2,7 +2,14 @@ package de.tlongo.serveranalytics.services.logfileservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManagerFactory;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.DirectoryStream;
@@ -33,15 +40,35 @@ import static spark.Spark.*;
         - /entries/currentday --> Gets all log entries from today
         - /entries/{date}     --> Gets log entries of a specific date
  */
+@Service
 public class LogService {
-    static Logger logger = LoggerFactory.getLogger(LogService.class);
-    static Properties properties;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    Logger logger = LoggerFactory.getLogger(LogService.class);
+    Properties properties;
+
+    @Autowired
+    LogEntryRepository dao;
 
     public static void main(String[] args) throws IOException {
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("/springdataconfig.xml");
+
+        LogService service = springContext.getBean(LogService.class);
+        service.startService();
+    }
+
+    private void startService() throws IOException {
+        logger.info("Starting LogService");
+        logger.info("Init Spring Data");
+
+        if (dao == null) {
+            logger.error("Could not create repository for db access");
+            throw new RuntimeException("Could not create repository for db access");
+        }
+        logger.info("Spring is up and running");
+
         initProperties();
-        final ScheduledFuture<?> persistorHandle = scheduler.scheduleAtFixedRate(() -> System.out.println("Persisting..."), 0, 10, TimeUnit.SECONDS);
+        final ScheduledFuture<?> persistorHandle = scheduler.scheduleAtFixedRate(() -> persistLogEntries(), 10, 10, TimeUnit.SECONDS);
 
         get("/health", (request, response) -> {
             return "LogFileService alive";
@@ -53,17 +80,25 @@ public class LogService {
     }
 
 
-    static void persistLogEntries() throws IOException {
+    void persistLogEntries() {
         logger.info("Starting persisting log entries to db");
 
         String logdirPath = properties.getProperty("logfileservice.logdir");
+        logger.info("Logfile directory is {}", logdirPath);
 
+        logger.info("Parsing log files");
         List<LogEntry> logEntryList = LogFileParser.parseLogDirectory(logdirPath);
 
-        // persist List to db
+
+        logger.info("Persisting {} entries to db.", logEntryList.size());
+        logEntryList.forEach(entry -> {
+            dao.save(entry);
+        });
+
+        logger.info("Done persisting log entries");
     }
 
-    private static void initProperties() throws IOException {
+    private void initProperties() throws IOException {
         URL url = LogService.class.getClassLoader().getResource("config.properties");
         properties = new Properties();
         BufferedInputStream stream = new BufferedInputStream(new FileInputStream(url.getFile()));
