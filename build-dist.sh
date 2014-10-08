TomasLongo/logfileservice

commit 06f13bc27b98bedcb8b04ea2b53556f3475a947d

Changed configs for production to use MySQL

src/main/resources/springdataconfig.xml
@@ -1,27 +1,22 @@
 <beans xmlns="http://www.springframework.org/schema/beans"
        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
        xmlns:jpa="http://www.springframework.org/schema/data/jpa"
-       xmlns:context="http://www.springframework.org/schema/context"
        xsi:schemaLocation="http://www.springframework.org/schema/beans
     http://www.springframework.org/schema/beans/spring-beans.xsd
     http://www.springframework.org/schema/data/jpa
-    http://www.springframework.org/schema/data/jpa/spring-jpa.xsd
-    http://www.springframework.org/schema/context
-    http://www.springframework.org/schema/context/spring-context.xsd">
+    http://www.springframework.org/schema/data/jpa/spring-jpa.xsd">
 
-    <context:annotation-config/>
-    <context:component-scan base-package="de.tlongo.serveranalytics.services"/>
     <jpa:repositories base-package="de.tlongo.serveranalytics.services.logfileservice"/>
 
     <bean id="dataSource" class="org.springframework.jdbc.datasource.DriverManagerDataSource">
-        <property name="driverClassName" value="org.postgresql.Driver"/>
-        <property name="url" value="jdbc:postgresql://localhost:5432/test"/>
-        <property name="username" value="test"/>
-        <property name="password" value="test"/>
+        <property name="driverClassName" value="com.mysql.jdbc.Driver"/>
+        <property name="url" value="jdbc:mysql://localhost/analytics"/>
+        <property name="username" value="serveranalytics"/>
+        <property name="password" value="serveranalytics"/>
     </bean>
 
     <bean id="jpaVendorAdapter" class="org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter">
-        <property name="databasePlatform" value="org.hibernate.dialect.PostgreSQLDialect" />
+        <property name="databasePlatform" value="org.hibernate.dialect.MySQLDialect" />
         <property name="showSql" value="true"/>
         <property name="generateDdl" value="true"/>
         <!--<property name="database" value="postgresql"/>-->

commit 33cd8948ba4b700057cdd21970960fc0b69648ff

Fixed spring-config that was broken with commit 06f13bc27b98bedcb8b04ea2b53556f3475a947d

build-dist.sh
@@ -1,1 +1,1 @@
-gradle --daemon clean installApp -x ProcessResources
+gradle --daemon clean test distZip -x ProcessResources

src/main/resources/config.properties
@@ -2,4 +2,5 @@ logfileservice.logdir=/home/tomas/projects/serveranalytics/src/test/logdir
 
 logfileservice.persisting.perLogFile=true
 
+# In hours
 logfileservice.scanintervall=24

src/main/resources/springdataconfig.xml
@@ -1,11 +1,16 @@
 <beans xmlns="http://www.springframework.org/schema/beans"
        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
        xmlns:jpa="http://www.springframework.org/schema/data/jpa"
+       xmlns:context="http://www.springframework.org/schema/context"
        xsi:schemaLocation="http://www.springframework.org/schema/beans
     http://www.springframework.org/schema/beans/spring-beans.xsd
     http://www.springframework.org/schema/data/jpa
-    http://www.springframework.org/schema/data/jpa/spring-jpa.xsd">
+    http://www.springframework.org/schema/data/jpa/spring-jpa.xsd
+    http://www.springframework.org/schema/context
+    http://www.springframework.org/schema/context/spring-context.xsd">
 
+    <context:annotation-config/>
+    <context:component-scan base-package="de.tlongo.serveranalytics.services"/>
     <jpa:repositories base-package="de.tlongo.serveranalytics.services.logfileservice"/>
 
     <bean id="dataSource" class="org.springframework.jdbc.datasource.DriverManagerDataSource">